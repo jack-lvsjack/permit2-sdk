3db2e55d876dffef9150697e163fe8f2e7151735
Jack Yin <lvsjack@qq.com>
2024-03-13 14:16:54 +0800

    feat: multiple PERMIT2_ADDRESS

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,17 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+const PERMIT2_CONFIGS:{[key: number]: string} = {
+  // zKatana
+  [1261120]: '0x629B419d4601Bfa96E954C1B127fEC5efC3DCC20',
+  // astar zkEVM
+  [3776]:'0x000000000022D473030F116dDEE9F6B43aC78BA3'
+}
+
+// export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+export const PERMIT2_ADDRESS = (chainId: number): string => {
+  if (!(chainId in PERMIT2_CONFIGS)) throw new Error(`PERMIT2 not deployed on chain ${chainId}`)
+  return PERMIT2_CONFIGS[chainId]
+}
 
 export const MaxUint48 = BigNumber.from('0xffffffffffff')
 export const MaxUint160 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')
